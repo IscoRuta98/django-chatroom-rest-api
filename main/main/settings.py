cec04223522dbdde89828325f95e9250ee83bdff
Francisco Rutayebesibwa <fmruta98@gmail.com>
2023-11-10 18:33:43 +0200

    feat: add docker & postgres

--- main/main/settings.py
+++ main/main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("DB_DRIVER", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("PG_DB", "chat_api"),
+        "USER": os.environ.get("PG_USER", "postgres"),
+        "PASSWORD": "123456",
+        "HOST": os.environ.get("PG_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("PG_PORT", "5432"),
     }
 }
 
